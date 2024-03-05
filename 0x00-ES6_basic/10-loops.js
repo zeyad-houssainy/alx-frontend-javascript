92f0a51792738c5d4e86492be837bd6bbfdc3ae6
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-05 15:00:01 +0300

    1

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const res = [];
-  for (const idx of array) {
-    res.push(appendString + idx);
-  }
-
-  return res;
-}
--- 0x00-ES6_basic/11-createEmployeesObject.js
+++
@@ -1,5 +0,0 @@
-export default function createEmployeesObject(departmentName, employees) {
-  const obj = {};
-  obj[departmentName] = employees;
-  return obj;
-}
