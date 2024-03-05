46d44d127882fa835c258a718e9683032e05e2b1
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-05 10:52:30 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    /* eslint-disable no-unused-vars */
-    const task = true;
-    const task2 = false;
-    /* eslint-enable no-unused-vars */
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
