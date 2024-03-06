fb1931479a05b2c021fa9c082b1721cf25471233
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-06 22:42:01 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 export default function getResponseFromAPI() {
-  return new Promise(() => {});
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve("success");
+    } else {
+      reject(new Error("failed"));
+    }
+  });
 }
