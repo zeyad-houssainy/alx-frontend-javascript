2faf554bccc6b89701a13b44a5b8ac3137fb7b9b
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-13 23:01:35 +0300

    2

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const classroom1 = new ClassRoom(19);
+  const classroom2 = new ClassRoom(20);
+  const classroom3 = new ClassRoom(34);
+  return [classroom1, classroom2, classroom3];
+}
+export default initializeRooms;
---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._lenght = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._lenght;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._lenght = newLength;
+    } else {
+      throw new Error('length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set student(newStudent) {
+    if (Array.isArray(newStudent)) {
+      this._students = newStudent;
+    } else {
+      throw new Error('Students must be an array');
+    }
+  }
+
+}
+export default HolbertonCourse;
