3b22207dc433a8e72ea290b1388a337103f61a90
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-14 03:22:46 +0300

    Update 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+class Currency {
+  static checker(attribute, type) {
+    if (type === 'string' && typeof (attribute) !== 'string') {
+      throw new Error(`Name must be a ${type}`);
+    }
+    return true;
+  }
+
+  constructor(code, name) {
+    Currency.checker(code, 'string');
+    Currency.checker(name, 'string');
+    this._name = name;
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    Currency.checker(newCode, 'string');
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._code;
+  }
+
+  set name(newName) {
+    Currency.checker(newName, 'string');
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+export default Currency;
