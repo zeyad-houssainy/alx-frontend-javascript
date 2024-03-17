89a597fa9e86ef0283ddf122500ade3a8eb70b5e
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-17 21:38:58 +0300

    JS is cool

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,27 @@
+class Building {
+  static checker(attribute, type) {
+    if ((type === 'string' && typeof (attribute) !== 'string')
+    || (type === 'number' && typeof (attribute) !== 'number')
+    || (type === 'array' && !Array.isArray(attribute))
+
+    ) {
+      throw new Error(`Name must be a ${type}`);
+    }
+    return true;
+  }
+
+  constructor(sqft) {
+    Building.checker(sqft, 'number');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    Building.checker(newSqft, 'number');
+    this._sqft = newSqft;
+  }
+}
+export default Building;
