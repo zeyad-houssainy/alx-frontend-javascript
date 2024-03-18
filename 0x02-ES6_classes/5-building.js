972ac88d35586960f098f3b898a663c0195d8d12
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-18 16:07:24 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,28 +1,28 @@
 class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
-      }
+  static checker(attribute, type) {
+    if ((type === 'string' && typeof (attribute) !== 'string')
+    || (type === 'number' && typeof (attribute) !== 'number')
+    || (type === 'array' && !Array.isArray(attribute))
+
+    ) {
+      throw new Error(`Name must be a ${type}`);
     }
-    this.sqft = sqft;
+    return true;
   }
 
-  /**
-   * @param {Number} sqft
-   */
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft nust be a number');
-    }
+  constructor(sqft) {
+    Building.checker(sqft, 'number');
     this._sqft = sqft;
+    this.evacuationWarningMessage = () => {};
   }
 
   get sqft() {
     return this._sqft;
   }
-}
 
+  set sqft(newSqft) {
+    Building.checker(newSqft, 'number');
+    this._sqft = newSqft;
+  }
+}
 export default Building;
